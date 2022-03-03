2257499e65ce0044108907baf26c63330e515d24
Fred Chen <chentj070921@yeah.net>
2022-03-03 13:06:57 +0800

    CU-21pbxpy - Implement the API: get&add hive nodes

--- src/modules/hivehub/hivehub.py
+++ src/modules/hivehub/hivehub.py
@@ -3,8 +3,14 @@
 """
 The entrance for hive_hub module.
 """
+from bson import ObjectId
+
 from src import hive_setting
+from src.utils.db_client import cli
 from src.utils.http_response import hive_restful_response
+from src.utils_v1.constants import DID_INFO_DB_NAME
+
+HIVE_HUB_NODES = 'hive_hub_nodes'
 
 
 class HiveHub:
@@ -12,21 +18,23 @@ class HiveHub:
         pass
 
     @hive_restful_response
-    def get_version(self):
-        """ This value comes from tag name and must be '***v<major>.<minor>.<patch>' or '<major>.<minor>.<patch>' """
-        src = hive_setting.HIVE_VERSION
-        index = src.rfind('v')
-        if index >= 0:
-            src = src[index + 1:]
-        parts = src.split('.')
+    def get_nodes(self, nid, owner_did):
+        col_filter = {}
+        if nid:
+            col_filter['_id'] = ObjectId(nid)
+        if owner_did:
+            col_filter['owner_did'] = owner_did
+        nodes = cli.find_many_origin(DID_INFO_DB_NAME, HIVE_HUB_NODES, col_filter,
+                                     create_on_absence=True, throw_exception=False)
+
+        def node_mapper(n):
+            n['_id'] = str(n['_id'])
+            return n
         return {
-            'major': int(parts[0]),
-            'minor': int(parts[1]),
-            'patch': int(parts[2]),
+            'nodes': list(map(node_mapper, nodes))
         }
 
     @hive_restful_response
-    def get_commit_id(self):
-        return {
-            'commit_id': hive_setting.HIVE_COMMIT_HASH
-        }
+    def add_node(self, node):
+        return cli.insert_one_origin(DID_INFO_DB_NAME, HIVE_HUB_NODES, node,
+                                     create_on_absence=True, is_extra=False)
--- src/view/hivehub.py
+++ src/view/hivehub.py
@@ -3,7 +3,7 @@
 """
 hive_hub module to save hive node list in hive hub website.
 """
-from flask import Blueprint, request
+from flask import Blueprint
 
 from src.modules.hivehub.hivehub import HiveHub
 from src.utils.http_exception import InvalidParameterException
@@ -22,9 +22,8 @@ def init_app(app):
 
 @blueprint.route('/api/hivehub/nodes', methods=['GET'])
 def get_nodes():
-    hive1_url = 'https://hive1.trinity-tech.io:443'
-    owner_did = 'did:elastos:ikkFHgoUHrVDTU8HTYDAWH9Z8S377Qvt7o'
-    nodes = [{
+    """ query nodes by nid(_id) or owner_did
+    Return: [{
         "nid": "jlaksjdflkjasdlkfj001",
         "name": "hive node 节点A",
         "created": "2021-11-09 21:00:32",
@@ -44,41 +43,29 @@ def get_nodes():
         "email": "1234456789@gamil.com",
         "url": hive1_url,
         "remark": "区块链是一个信息技术领域的术语。从本质上讲，它是一个共享数据库，存储于其中的数据或信息，具有“不可伪造”“全程留痕”“可以追溯”“公开透明”“集体维护”等特征。基于这些特征，区块链技术奠定了坚实的“信任”基础，创造了可靠的“合作”机制，具有广阔的运用前景。"
-    }, {
-        "nid": "jlaksjdflkjasdlkfj003",
-        "name": "hive node 节点C",
+    }]
+    """
+    nid, msg = rqargs.get_str('nid')
+    owner_did, msg = rqargs.get_str('owner_did')
+    return hive_hub.get_nodes(nid, owner_did)
+
+
+@blueprint.route('/api/hivehub/node', methods=['POST'])
+def add_node():
+    """ Add node by `node` document
+    Example: {
+        "nid": "jlaksjdflkjasdlkfj001",
+        "name": "hive node 节点A",
         "created": "2021-11-09 21:00:32",
         "ip": "192.115.24.2",
         "owner_did": owner_did,
         "area": "加拿大 安大略省 多伦多市",
         "email": "1234456789@gamil.com",
-        "url": "http://localhost:9999",
+        "url": hive1_url,
         "remark": "区块链是一个信息技术领域的术语。从本质上讲，它是一个共享数据库，存储于其中的数据或信息，具有“不可伪造”“全程留痕”“可以追溯”“公开透明”“集体维护”等特征。基于这些特征，区块链技术奠定了坚实的“信任”基础，创造了可靠的“合作”机制，具有广阔的运用前景。"
-    }, {
-        "nid": "jlaksjdflkjasdlkfj004",
-        "name": "hive node 节点D",
-        "created": "2021-11-09 21:00:32",
-        "ip": "192.115.24.2",
-        "owner_did": "did:elastos:srgsve5h5yvnwi5yh4hyg2945hvwq0tq",
-        "area": "加拿大 安大略省 多伦多市",
-        "email": "1234456789@gamil.com",
-        "url": "http://localhost:9999",
-        "remark": "区块链是一个信息技术领域的术语。从本质上讲，它是一个共享数据库，存储于其中的数据或信息，具有“不可伪造”“全程留痕”“可以追溯”“公开透明”“集体维护”等特征。基于这些特征，区块链技术奠定了坚实的“信任”基础，创造了可靠的“合作”机制，具有广阔的运用前景。"
-    }]
-    nid, msg = rqargs.get_str('nid')
-    result = nodes
-    if nid and not msg:
-        result = list(filter(lambda n: n['nid'] == nid, result))
-    odid, msg = rqargs.get_str('owner_did')
-    if odid and not msg:
-        result = list(filter(lambda n: n['owner_did'] == odid, result))
-    return {"nodes": result}
-
-
-@blueprint.route('/api/hivehub/node', methods=['POST'])
-def add_node():
+    }
+    """
     node, msg = params.get_dict('node')
     if not node or msg:
         return InvalidParameterException(msg=msg).get_error_response()
-    print(f'TODO: add node: {node}')
-    return {}
+    return hive_hub.add_node(node)
